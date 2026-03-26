be610aff60c0e3b5de9b85c3bec366b335185db0
Marek Miklušek <marek.miklusek@gmail.com>
2026-03-26 15:02:09 +0100

    test: fix

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -13,8 +13,8 @@ test('to array', function (): void {
             'name',
             'email',
             'email_verified_at',
+            'two_factor_confirmed_at',
             'created_at',
             'updated_at',
-            'two_factor_confirmed_at',
         ]);
 });
