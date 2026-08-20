ece04d031c91465b77c6576087150d9f226a22e8
Marek Miklušek <marek.miklusek@gmail.com>
2026-08-20 16:06:59 +0200

    deps: upgrade PHP to version 8.5

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Pdo\Mysql;
 use Illuminate\Support\Str;
 
 return [
@@ -61,7 +62,7 @@ return [
             'strict' => true,
             'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
@@ -81,7 +82,7 @@ return [
             'strict' => true,
             'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
